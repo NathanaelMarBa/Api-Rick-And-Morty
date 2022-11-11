665ab3925fd5869a52113fb8ef85ecf71d8b459b
NathanaelMarBa <kevin_1321124060@uptecamac.edu.mx>
2022-11-11 16:49:57 -0600

    Pagina

---
+++ Personaje.PHP
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="Styles.css">
+    <title>Personaje del capitulo</title>
+</head>
+<body>
+
+<?php
+require('Funciones.PHP');
+if(isset($_GET["id"])){
+    $id = $_GET["id"];
+    $personaje = obtenerJson("https://rickandmortyapi.com/api/character/$id");
+    echo "Nombre: $personaje->name <br>";
+}
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
